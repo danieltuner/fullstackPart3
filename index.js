4f20a12664eb4ce6952b7cde5feba0dd10345edd
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-10-08 12:01:35 +0300

    3.1 done

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const express = require('express')
 const app = express()
+{
+let persons = [
+    {
+      "name": "Arto Hellas",
+      "number": "040-1",
+      "id": 1
+    },
+    {
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": 3
+    },
+    {
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122",
+      "id": 4
+    }
+  ]
 
-let notes = [
-  ...
-]
 
 
-
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+}
