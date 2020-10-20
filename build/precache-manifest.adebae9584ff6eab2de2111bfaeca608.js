f7354a31f2639c00e4a631bb43f0073ef85eceb4
Daniel Tunér <tuner.daniel@hotmail.com>
2020-10-20 13:04:20 +0300

    uibuild

---
+++ build/precache-manifest.adebae9584ff6eab2de2111bfaeca608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d74011636cc925cb44f8e632153effd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb0fd608aa8ad3441d88",
+    "url": "/static/css/main.326d9d8f.chunk.css"
+  },
+  {
+    "revision": "54dd32709c192453a110",
+    "url": "/static/js/2.d98781c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d98781c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb0fd608aa8ad3441d88",
+    "url": "/static/js/main.4eb8e559.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
