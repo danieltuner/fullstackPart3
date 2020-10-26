2afacd1a7a0b87e102984a65003ddfd923dd1db1
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-10-26 11:30:08 +0200

    3.15-3.18

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ app.use(cors())
 app.use(express.static('build'))
 
 const mongoose = require('mongoose')
+const { request, response } = require('express')
 
 
-morgan.token('body', (req, res) => {
-  if (req.method === 'POST') {
-    return JSON.stringify(req.body)
+morgan.token('body', (request, response) => {
+  if (request.method === 'POST') {
+    return JSON.stringify(request.body)
   }
   else {
     return null
@@ -23,12 +24,6 @@ morgan.token('body', (req, res) => {
 })
 
 {
-
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
 
 
 app.get('/api/persons', (request, response) => {
@@ -39,9 +34,11 @@ app.get('/api/persons', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-    const info = `Phonebook has information of <b>${persons.length}</b> people.`
-    const time = Date()
-    response.send(`<p>${info}</p><p>${time}</p>`)
+    Person.find({}).then(people => {
+      const info = `Phonebook has information of ${people.length} people.`
+      const time = Date()
+      res.send(`<p>${info}</p><p>${time}</p>`)
+    })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -54,6 +51,7 @@ app.get('/api/persons/:id', (request, response, next) => {
           response.status(404).end()
         }
       })
+      .catch(error => next(error))
     
 })
 
@@ -63,6 +61,21 @@ app.delete('/api/persons/:id', (request, response, next) => {
     response.status(204).end()
   })
   .catch(error => next(error))
+  })
+
+  app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+      name: body.name,
+      number: body.number,
+    }
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
   })
 
 app.post('/api/persons', (request, response) => {
@@ -78,22 +91,34 @@ app.post('/api/persons', (request, response) => {
         error: 'The number is missing'
       })
     }
-    if (!persons.every(p => p.name !== body.name)) {
-      return response.status(400).json({
-          error: 'The name already exists in the phonebook'
-      })
-    }
-    
-
-    const person = {
+        
+    const person = new Person({
         id: Math.floor(Math.random() * 5000),
         name: body.name,
         number: body.number,
-    }
+    })
     
-    persons = persons.concat(person)
-    response.json(person)
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
 })
+  const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+      return response.status(400).send({error: 'malformatted id'})
+    }
+    next(error)
+  }
+  app.use(errorHandler)
+
+  const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+  }
+
+  app.use(unknownEndpoint)
+
+
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
