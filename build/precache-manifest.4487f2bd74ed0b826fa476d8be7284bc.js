dbf42d7cba1da5b1166ffc7dd7de51c2837f45be
Daniel Tunér SOFTK20P <daniel.tuner@centria.fi>
2020-10-27 11:05:54 +0200

    a build

---
+++ build/precache-manifest.4487f2bd74ed0b826fa476d8be7284bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b34b3ae788ba92ed69ac21d7e826956",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abe09b286d497a6b14d6",
+    "url": "/static/css/main.326d9d8f.chunk.css"
+  },
+  {
+    "revision": "54dd32709c192453a110",
+    "url": "/static/js/2.d98781c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d98781c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe09b286d497a6b14d6",
+    "url": "/static/js/main.ebabdd36.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
